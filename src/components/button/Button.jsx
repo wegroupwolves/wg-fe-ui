cf1172841d04d94a47e9f0476c3481558cd185d7
Maxime Verhoeve <maxime.verhoeve@wegroup.be>
2019-09-02 15:31:58 +0200

    ✨ initialized storybook with first component

---
+++ src/components/button/Button.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import {bool, string} from 'prop-types';
+
+const Button = ({color, disabled, label}) => {
+ let colorKey;
+
+ switch (color) {
+  case 'primary':
+   colorKey = '#FF8000'
+   break;
+  case 'secondary':
+   colorKey = '#DEDEDE'
+   break;
+ 
+  default:
+   break;
+ }
+ return (
+  <div>
+   <button disabled={disabled} style={{backgroundColor: colorKey, color: 'white', border: 0, padding: '0.5rem 2rem', borderRadius: '0.6rem', fontSize: '1.2rem', fontWeight: '900'}} >{label}</button>
+  </div>
+ )
+}
+
+Button.defaultProps = {
+ disabled: false,
+ color: 'red',
+ label: 'button',
+};
+
+Button.propTypes = {
+ disabled: bool,
+ color: string,
+ label: string,
+}
+
+export default Button
