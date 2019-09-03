d5f5a601ed14ebc7c1f19e8a450317b56f7c9386
Maxime Verhoeve <maxime.verhoeve@wegroup.be>
2019-09-03 09:37:24 +0200

    ✨ added webpack to fix errors

--- src/components/button/Button.js
+++ src/components/button/Button.js
@@ -17,7 +17,7 @@ const Button = ({color, disabled, label}) => {
  }
  return (
    <button disabled={disabled} style={{backgroundColor: colorKey, color: 'white', border: 0, padding: '0.5rem 2rem', borderRadius: '0.6rem', fontSize: '1.2rem', fontWeight: '900'}} >{label}</button>
- );
+ )
 }
 
 Button.defaultProps = {
