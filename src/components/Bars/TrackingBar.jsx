a1b14659c1902a642017bf33db8b26e535c6969a
Aaron Israel <aaron.israel@wegroup.be>
2019-10-08 12:00:09 +0200

    ✨Added classNames to progressbar and trackingbar

--- src/components/Bars/TrackingBar.jsx
+++ src/components/Bars/TrackingBar.jsx
@@ -1,13 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
 import { key } from 'styled-theme/dist';
-import ApprovedIcon from './../Icons/Approved';
-import OpenedIcon from './../Icons/Opened';
-import FilingIcon from './../Icons/Filing';
-import LoupeIcon from './../Icons/Loupe';
-import PlaneIcon from './../Icons/Plane';
-import PayIcon from './../Icons/Pay';
-import UploadingIcon from './../Icons/Uploading';
 import LoadingBar from './LoadingBar';
 import TrackingStep from './TrackingStep';
 import { arrayOf, node, number, string } from 'prop-types';
@@ -16,10 +9,11 @@ const getActiveId = (active, stages) => {
   return stages ? stages.findIndex(item => item.key === active) + 1 : 0;
 };
 
-const TrackingBar = ({ active, background, height, stages }) => {
+const TrackingBar = ({ active, background, height, stages, className }) => {
   const activeId = getActiveId(active, stages);
+
   return (
-    <StyledTrackingBar>
+    <StyledTrackingBar className={className}>
       <LoadingBar
         stages={stages.length}
         activeId={activeId}
@@ -53,50 +47,7 @@ TrackingBar.defaultProps = {
   active: 'data_analysis',
   background: key('colors.disabledGray'),
   height: '2px',
-  stages: [
-    {
-      key: 'filing',
-      name: 'filing',
-      icon: <FilingIcon />,
-      id: 1,
-    },
-    {
-      key: 'uploading_documents',
-      name: 'uploading documents',
-      icon: <UploadingIcon />,
-      id: 2,
-    },
-    {
-      key: 'data_analysis',
-      name: 'data analysis',
-      icon: <LoupeIcon />,
-      id: 3,
-    },
-    {
-      key: 'claim_opened',
-      name: 'claim opened',
-      icon: <OpenedIcon />,
-      id: 4,
-    },
-    {
-      key: 'air_carrier_processing',
-      name: 'air carrier processing',
-      icon: <PlaneIcon />,
-      id: 5,
-    },
-    {
-      key: 'approved',
-      name: 'approved',
-      icon: <ApprovedIcon />,
-      id: 6,
-    },
-    {
-      key: 'payout',
-      name: 'payout',
-      icon: <PayIcon />,
-      id: 7,
-    },
-  ],
+  stages: [],
 };
 
 TrackingBar.propTypes = {
@@ -109,6 +60,7 @@ TrackingBar.propTypes = {
     id: number,
   }),
   height: string,
+  className: string,
 };
 
 export default TrackingBar;
