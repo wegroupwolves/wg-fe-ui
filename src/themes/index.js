40f6e066e0c2b09a2c0875992076b4e8fb58fe16
Aaron Israel <aaron.israel@wegroup.be>
2019-12-09 11:55:34 +0100

    ✨Added light and oceanblue

--- src/themes/index.js
+++ src/themes/index.js
@@ -1,2 +1,4 @@
 export { default as orange } from './orange';
+export { default as lightBlue } from './lightBlue';
+export { default as oceanBlue } from './oceanBlue';
 export { default as elvas } from './elvas';
---
+++ src/themes/oceanBlue.js
@@ -0,0 +1,54 @@
+export default function() {
+  const brandPrimary = '#0077aa';
+  const brandLight = '#A5C6D5';
+  const brandLighter = '#99bce5';
+  const brandLightest = '#EDF3FA';
+  const brandDark = '#24558f';
+  const brandDarker = '#003768';
+  const brandSecondary = '#A5C6D5';
+  const brandFont = `Lato, Helvetica, Myriad, Sans-Serif`;
+  return {
+    brand: {
+      primary: brandPrimary,
+      light: brandLight,
+      lighter: brandLighter,
+      lightest: brandLightest,
+      dark: brandDark,
+      darker: brandDarker,
+      secondary: brandSecondary,
+    },
+    typo: {
+      title: '#050505',
+      text: '#0E0E0E',
+      highlight: '#505050',
+      subTitle: '#5B5550',
+      interactive: '#C1C1C1',
+      outline: '#CCCCCC',
+      editable: '#3297FD',
+      selected: brandLight,
+    },
+    ui: {
+      background: '#FBFBFB',
+      backgroundLight: '#D3D4D8',
+      toolbar: '#F6F6F6',
+      outline: '#CCCCCC',
+      interactive: '#C1C1C1',
+      disabled: '#AEAEAE',
+      highlight: '#505050',
+      active: brandLight,
+    },
+    hover: {
+      primary: brandLightest,
+      secondary: '#F6F6F6',
+    },
+    status: {
+      succes: '#37D66C',
+      error: '#F74040',
+      warning: '#FF9B21',
+      succesLight: '#D2FDE0',
+      errorLight: '#FFE1E1',
+      warningLight: '#FFEFDC',
+    },
+    font: brandFont,
+  };
+}
