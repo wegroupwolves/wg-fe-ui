c7d5ce4deeb5e2b1fd49d05598c14064bc13dd5e
Piotr <piotr.winiarski@300brains.com>
2019-12-09 13:37:44 +0100

    AsyncSelect componented added, test for it written and added to storybook

--- src/components/Inputs/SearchSelectInput.test.js
+++ src/components/Inputs/SearchSelectInput.test.js
@@ -4,6 +4,7 @@ import React from 'react';
 import 'jest-styled-components';
 import { ThemeProvider } from '../..';
 import { orange } from '../../themes';
+import { act } from 'react-dom/test-utils';
 const theme = orange();
 
 // eslint-disable-next-line
@@ -18,12 +19,12 @@ export const mountWithTheme = tree =>
 
 describe('SearchSelectInput', () => {
   it('returns value and name of selected option', () => {
-    let testValue = { name: null, value: null };
+    let testValue = { label: null, value: null };
     const wrapper = mountWithTheme(
       <SearchSelectInput
         name="test"
-        onSelected={({ name, value }) => {
-          (testValue.name = name), (testValue.value = value);
+        onSelected={({ label, value }) => {
+          (testValue.label = label), (testValue.value = value);
         }}
         options={[
           { value: 'option1', label: 'Option 1' },
@@ -45,8 +46,9 @@ describe('SearchSelectInput', () => {
       .simulate('keyDown', { key: 'Enter', keyCode: 13 });
 
     // if simulate is complete, onSelected should be triggered
+    expect(wrapper.props().name).toEqual('test');
     expect(testValue.value).toEqual('option1');
-    expect(testValue.name).toEqual('test');
+    expect(testValue.label).toEqual('Option 1');
   });
   it('placeholder should be loading when enabled', () => {
     const wrapper = mountWithTheme(
@@ -109,7 +111,12 @@ describe('SearchSelectInput', () => {
     );
 
     // check if input prop disabled is false by default
-    expect(wrapper.find('input').props().disabled).toEqual(false);
+    expect(
+      wrapper
+        .find('input')
+        .at(0)
+        .props().disabled,
+    ).toEqual(false);
 
     // check if label is sub-title color
     expect(wrapper.find('label')).toHaveStyleRule('color', theme.typo.subTitle);
@@ -147,5 +154,41 @@ describe('SearchSelectInput', () => {
     );
 
     expect(wrapper.find('Select').props().id).toEqual(12);
+  });
+  it('returns value and name of selected option', () => {
+    let testValue = { label: null, value: null };
+    const wrapper = mountWithTheme(
+      <SearchSelectInput
+        async
+        name="test"
+        onSelected={({ label, value }) => {
+          (testValue.label = label), (testValue.value = value);
+        }}
+        loadOptions={() => [
+          { value: 'option1', label: 'Option 1' },
+          { value: 'option2', label: 'Option 2' },
+        ]}
+      >
+        Test
+      </SearchSelectInput>,
+    );
+
+    // simulate selecting the first option
+    act(() => {
+      wrapper
+        .find('.Select__control')
+        .at(1)
+        .simulate('keyDown', { key: 'ArrowDown', keyCode: 40 });
+      wrapper
+        .find('.Select__control')
+        .at(1)
+        .simulate('keyDown', { key: 'Enter', keyCode: 13 });
+    });
+
+    setTimeout(() => {
+      // if simulate is complete, onSelected should be triggered
+      expect(testValue.value).toEqual('option1');
+      expect(testValue.label).toEqual('Option 1');
+    });
   });
 });
