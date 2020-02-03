3590ebc05a309d3f5dd316d970ca635bfe9d5783
DDobrowolski <damian.dobrowolski@300brains.com>
2020-02-03 17:34:53 +0100

    Made few components more responsive

--- src/components/Bars/ToolBar.jsx
+++ src/components/Bars/ToolBar.jsx
@@ -21,13 +21,16 @@ const Wrapper = styled.div`
   justify-content: space-between;
 `;
 
+const ButtonWrapper = styled.div`
+  border-left: 1px solid ${({ theme }) => theme.ui.outline};
+  padding-left: 2rem;
+`;
+
 const InnerBox = styled.div`
   position: relative;
   display: flex;
   align-items: center;
-  width: 128rem;
   height: 100%;
-  border-right: 1px solid ${({ theme }) => theme.ui.outline};
 
   input {
     border: none;
@@ -50,14 +53,16 @@ const ToolBar = ({
     <Box className={className} {...otherProps}>
       <Wrapper>
         <InnerBox>{children}</InnerBox>
-        <ActionButton
-          disabled={btnDisable}
-          level={level}
-          onClick={onClick}
-          padding="0.5rem 1.5rem"
-        >
-          {btnName}
-        </ActionButton>
+        <ButtonWrapper>
+          <ActionButton
+            disabled={btnDisable}
+            level={level}
+            onClick={onClick}
+            padding="0.5rem 1.5rem"
+          >
+            {btnName}
+          </ActionButton>
+        </ButtonWrapper>
       </Wrapper>
     </Box>
   );
