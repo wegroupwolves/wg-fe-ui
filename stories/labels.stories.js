1892d62261c3cccff832abd28273e22a8591c3a5
Gilles <me@oshe.be>
2020-03-04 09:19:09 +0100

    ✨ Risk object labels added

--- stories/labels.stories.js
+++ stories/labels.stories.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { storiesOf, addParameters } from '@storybook/react';
 import { withInfo } from '@storybook/addon-info';
-import { withKnobs, select, text } from '@storybook/addon-knobs';
-// import { boolean } from '@storybook/addon-knobs';
+import { withKnobs, select, text, number } from '@storybook/addon-knobs';
+import * as Icons from '../src/components/Icons';
 
 addParameters({
   options: {
@@ -17,7 +17,16 @@ const LabelTypes = {
   Lead: 'lead',
 };
 
-import TypeLabel from '../src/components/Labels/TypeLabel.jsx';
+const IconsKeys = Object.keys(Icons);
+const IconsList = {};
+IconsKeys.map(IconKey => {
+  IconsList[Icons[IconKey].name] = Icons[IconKey].name;
+});
+
+import TypeLabel from '../src/components/Labels/TypeLabel';
+import RiskObjectLabel from '../src/components/Labels/RiskObjectLabel';
+
+console.log(IconsKeys[0]);
 
 storiesOf('Low level blocks/Labels', module)
   .addDecorator(withKnobs)
@@ -25,8 +34,16 @@ storiesOf('Low level blocks/Labels', module)
   .add('TypeLabel', () => {
     return (
       <TypeLabel
-        type={select('Test', LabelTypes)}
+        type={select('Label type', LabelTypes)}
         text={text('Label text', 'Prospect')}
       ></TypeLabel>
+    );
+  })
+  .add('RiskObjectLabel', () => {
+    return (
+      <RiskObjectLabel
+        amount={number('Amount', 0)}
+        icon={select('Icon', IconsList, IconsKeys[0])}
+      ></RiskObjectLabel>
     );
   });
