c1d8dc546eb229554a09aadf6e79e1a709c1e10f
Gilles <gilles.demuynck@wegroup.be>
2020-03-10 12:02:51 +0100

    🐛 AddEntityButton reworked & hover added

--- src/components/Buttons/AddEntityButton.jsx
+++ src/components/Buttons/AddEntityButton.jsx
@@ -1,55 +1,67 @@
 import React from 'react';
 import { object, string, element, func } from 'prop-types';
 import styled from 'styled-components';
-import PlusIcon from './../Icons/Plus';
+import * as Icons from '../Icons';
 
-const AddEntityButton = ({ icon, onClick, name, ...otherProps }) => {
+const IconsList = {};
+Object.keys(Icons).map(IconKey => {
+  IconsList[Icons[IconKey].name] = Icons[IconKey];
+});
+
+const AddEntityButton = ({ fullwidth, icon, onClick, name, ...otherProps }) => {
+  const ChosenIcon = IconsList[icon];
+
   return (
-    <StyledButton onClick={onClick} {...otherProps}>
-      {icon}
-      {name}
+    <StyledButton fullwidth={fullwidth} onClick={onClick} {...otherProps}>
+      <ChosenIcon size={40}></ChosenIcon>
+      <span className="label">{name}</span>
     </StyledButton>
   );
 };
 
 const StyledButton = styled.button`
-  font-family: ${({ theme }) => theme.font};
-  font-size: 1.6rem;
-  line-height: 130%;
-  border-radius: 0.5rem;
-  min-width: 14rem;
-  min-width: 250px;
-  padding: 2vh 0;
-  border: 0.5px solid;
-  border-color: ${({ theme }) => theme.ui.outline};
-  box-sizing: border-box;
-  border-radius: 5px;
+  min-width: 25rem;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  color: #505050;
+  padding: 2rem;
+  border-radius: 0.5rem;
+  border: 2px dashed ${({ theme }) => theme.ui.outline};
+  background-color: #f0f1f3;
   cursor: pointer;
-  background-color: ${({ theme }) => theme.ui.background};
-  position: relative;
-
-  :active {
-    transform: scale(0.95);
-  }
-
-  :focus {
-    outline: none;
-  }
-
-  :hover {
-    /* background-color: #FFBF80; */
-  }
+  transition: 0.15s ease-in-out;
+  width: ${({ fullwidth }) => (fullwidth ? '100%' : 'auto')};
 
   svg {
-    margin-bottom: 12px;
+    path {
+      stroke: ${({ theme }) => theme.ui.outline};
+      fill: ${({ theme }) => theme.ui.outline};
+      transition: 0.15s ease-in-out;
+    }
+  }
 
-    &:hover line {
-      stroke: ${({ theme }) => theme.ui.interactive};
+  .label {
+    margin-top: 5px;
+    font-size: 1.4rem;
+    line-height: 130%;
+    color: ${({ theme }) => theme.typo.highlight};
+    transition: 0.15s ease-in-out;
+  }
+
+  &:hover {
+    border: 2px dashed ${({ theme }) => theme.brand.primary};
+    background-color: ${({ theme }) => theme.brand.lightest};
+
+    svg {
+      path {
+        stroke: ${({ theme }) => theme.brand.primary};
+        fill: ${({ theme }) => theme.brand.primary};
+      }
+    }
+
+    .label {
+      color: ${({ theme }) => theme.brand.primary};
     }
   }
 `;
@@ -57,13 +69,13 @@ const StyledButton = styled.button`
 StyledButton.displayName = 'StyledButton';
 
 AddEntityButton.defaultProps = {
-  icon: <PlusIcon />,
+  icon: 'IconPlusFilled',
   otherProps: {},
   fullwidth: false,
 };
 
 AddEntityButton.propTypes = {
-  icon: element,
+  icon: string,
   name: string.isRequired,
   onClick: func.isRequired,
   otherProps: object,
