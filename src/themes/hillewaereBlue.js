672e64a4deba67ff4845cc09a59710243a8fced3
Michaël Schauwers <michael@schauwe.rs>
2020-07-27 10:54:02 +0200

    ✨🎨Added Hillewaere theme

---
+++ src/themes/hillewaereBlue.js
@@ -0,0 +1,69 @@
+export default function() {
+  const brandPrimary = '#21193f';
+  const brandLight = '#5D8DCC';
+  const brandLighter = '#9baee4';
+  const brandLightest = '#c3ceef';
+  const brandDark = '#1b2e64';
+  const brandDarker = '#1B1433';
+  const brandSecondary = '#3989C4';
+  const brandFont = `Lato, Helvetica, Myriad, Sans-Serif`;
+  return {
+    brand: {
+      primary: brandPrimary,
+      light: brandLight,
+      lighter: brandLighter,
+      lightest: brandLightest,
+      dark: brandDark,
+      darker: brandDarker,
+      secondary: brandSecondary,
+    },
+    typo: {
+      title: '#050505',
+      text: '#0E0E0E',
+      highlight: '#505050',
+      subTitle: '#5B5550',
+      interactive: '#C1C1C1',
+      outline: '#CCCCCC',
+      editable: '#3297FD',
+      selected: brandLight,
+    },
+    ui: {
+      background: '#FBFBFB',
+      backgroundLight: '#D3D4D8',
+      toolbar: '#F6F6F6',
+      outline: '#CCCCCC',
+      interactive: '#C1C1C1',
+      disabled: '#AEAEAE',
+      highlight: '#505050',
+      active: brandLight,
+    },
+    hover: {
+      primary: brandLightest,
+      secondary: '#F6F6F6',
+    },
+    status: {
+      succes: '#37D66C',
+      error: '#F74040',
+      warning: '#FF9B21',
+      succesLight: '#D2FDE0',
+      errorLight: '#FFE1E1',
+      warningLight: '#FFEFDC',
+      blue: '#00A3FF',
+      yellow: '#FFD600',
+    },
+    labels: {
+      default: '#C1C1C1',
+      defaultText: '#505050',
+
+      prospect: '#E2EDF0',
+      prospectText: '#6EA3B4',
+
+      lead: '#FFF7CE',
+      leadText: '#C8893D',
+
+      customer: '#E3F0E2',
+      customerText: '#75B46E',
+    },
+    font: brandFont,
+  };
+}
