48039ca4b940de73a3e0cefbe8772f5c25863053
Sam Amant <sam.amant@wegroup.be>
2020-10-26 16:22:17 +0100

    :wrench: anti hide logic on hover

--- src/components/Boxes/NotificationPopUp.js
+++ src/components/Boxes/NotificationPopUp.js
@@ -1,7 +1,28 @@
-import React from 'react';
+// stylelint-disable value-keyword-case
+import React, { useEffect, useState } from 'react';
 import { string, object, func, element } from 'prop-types';
 import LinkHandler from '../Link';
-import styled, { keyframes } from 'styled-components';
+import styled from 'styled-components';
+
+const Timer = function(callback, delay) {
+  var timerId, start, remaining = delay;
+
+  this.pause = function() {
+      window.clearTimeout(timerId);
+      remaining -= Date.now() - start;
+  };
+
+  this.resume = function() {
+      start = Date.now();
+      window.clearTimeout(timerId);
+      timerId = window.setTimeout(callback, remaining);
+  };
+
+  this.resume();
+};
+
+let hideTimer;
+let hidePopUpFunctionTimer;
 
 const NotificationPopUp = ({
   title,
@@ -9,12 +30,38 @@ const NotificationPopUp = ({
   time,
   description,
   to,
+  hidePopup = () => {},
   onClick = () => {},
   className,
+  hideDelay = 10,
   otherProps
 }) => {
+  const [show, setShow] = useState(false);
+
+  useEffect(() => {
+    setShow(true);
+    hideTimer = new Timer(() => {
+      setShow(false);
+      hidePopUpFunctionTimer = setTimeout(() => {
+        hidePopup();
+      }, 500);
+    }, hideDelay * 1000);
+  }, []);
   return (
-    <Container className={className} {...otherProps} onClick={onClick} to={to}>
+    <Container
+      hideDelay={hideDelay}
+      show={show}
+      onMouseEnter={() => {
+        hideTimer.pause();
+        if (hidePopUpFunctionTimer) clearTimeout(hidePopUpFunctionTimer);
+        setShow(true);
+      }}
+      onMouseLeave={() => hideTimer.resume()}
+      className={className}
+      onClick={onClick}
+      to={to}
+      {...otherProps}
+    >
       {icon && (
         <IconContainer>
           <IconCircle>
@@ -36,16 +83,6 @@ const NotificationPopUp = ({
     </Container>
   );
 };
-
-const FadeIn = keyframes`
-  from {
-    opacity: 0;
-  }
-
-  to {
-    opacity: 1;
-  }
-`;
 
 const TimeLeft = styled.div`
   font-weight: 500;
@@ -71,6 +108,7 @@ const IconCircle = styled.div`
 `;
 
 const Container = styled(LinkHandler)`
+  box-sizing: border-box;
   display: flex;
   width: 100%;
   position: relative;
@@ -82,10 +120,21 @@ const Container = styled(LinkHandler)`
   justify-content: space-between;
   filter: drop-shadow(-4px 4px 12px rgba(0, 0, 0, 0.05));
   border-radius: 5px;
+  opacity: 1;
+  max-height: 15rem;
   cursor: pointer;
+  overflow: hidden;
   padding: 2rem;
-  animation: ${FadeIn} 200ms ease;
-  transition: all .2s ease;
+  transition: all 500ms ease;
+  transform: scaleY(1);
+  ${({ show }) => !show && `
+    opacity: 0;
+    max-height: 0;
+    transform: scaleY(0);
+    padding: 0;
+    border-color: rgba(0,0,0,0);
+    border-width: 0;
+  `}
 
   &:hover {
     border: 1px solid ${({ theme }) => theme.brand.primary};
--- src/components/Link.jsx
+++ src/components/Link.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link as ReactLink } from 'react-router-dom';
 
-const Link = ({ className, to, children, ...otherProps }) => {
+const Link = ({ className, to, children, forwardRef, ...otherProps }) => {
   const parseTo = to => {
     let parser = document.createElement('a');
     parser.href = to;
@@ -16,9 +16,9 @@ const Link = ({ className, to, children, ...otherProps }) => {
   const isInternal = isInternalCheck(toLocation);
 
   if (isInternal) {
-    return <ReactLink className={className} to={toLocation.pathname} {...otherProps}>{children}</ReactLink>;
+    return <ReactLink innerRef={forwardRef} className={className} to={toLocation.pathname} {...otherProps}>{children}</ReactLink>;
   } else {
-    return <a className={className} href={to} target="_blank" {...otherProps}>{children}</a>;
+    return <a ref={forwardRef} className={className} href={to} target="_blank" {...otherProps}>{children}</a>;
   }
 };
 
