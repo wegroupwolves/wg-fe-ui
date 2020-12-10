42a1c1472ca46a83e489f12c39f7aee1bf57c072
Michiel <michiel@wegroup.be>
2020-12-10 09:53:33 +0100

    Add rest props to accordionBox

--- src/components/Boxes/AccordionBox/AccordionBoxContent.jsx
+++ src/components/Boxes/AccordionBox/AccordionBoxContent.jsx
@@ -2,8 +2,12 @@ import React from 'react';
 import styled from 'styled-components';
 import { node, bool } from 'prop-types';
 
-const AccordionBoxContent = ({ children, open }) => {
-  return <Wrapper open={open}>{children}</Wrapper>;
+const AccordionBoxContent = ({ children, open, ...rest }) => {
+  return (
+    <Wrapper open={open} {...rest}>
+      {children}
+    </Wrapper>
+  );
 };
 
 const Wrapper = styled.div`
--- src/components/Boxes/AccordionBox/AccordionBoxHeader.jsx
+++ src/components/Boxes/AccordionBox/AccordionBoxHeader.jsx
@@ -3,9 +3,9 @@ import styled from 'styled-components';
 import { IconActionDropDown } from '../../Icons/index';
 import { node, bool, func } from 'prop-types';
 
-const AccordionBoxHeader = ({ children, open, toggle }) => {
+const AccordionBoxHeader = ({ children, open, toggle, ...rest }) => {
   return (
-    <Wrapper onClick={toggle}>
+    <Wrapper onClick={toggle} {...rest}>
       {children}
 
       <ToggleWrapper open={open}>
