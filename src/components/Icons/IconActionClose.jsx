1194120770e21cc8b0f118785e3c12d670dacc20
Michiel <michiel@wegroup.be>
2021-03-09 16:00:47 +0100

    Add rest props for passing extra props

--- src/components/Icons/IconActionClose.jsx
+++ src/components/Icons/IconActionClose.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { string } from 'prop-types';
 
-const IconActionClose = ({ className, color, size }) => {
+const IconActionClose = ({ className, color, size, ...rest }) => {
   return (
     <svg
       id="icons"
@@ -9,8 +9,8 @@ const IconActionClose = ({ className, color, size }) => {
       width={size}
       height={size}
       viewBox="0 0 50 50"
+      {...rest}
     >
-      
       <path
         d="M36 16.93L33.07 14L25 22.07L16.93 14L14 16.93L22.07 25L14 33.07L16.93 36L25 27.93L33.07 36L36 33.07L27.93 25L36 16.93Z"
         fill={color}
