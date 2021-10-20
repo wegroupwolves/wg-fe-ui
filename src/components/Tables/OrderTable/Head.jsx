08450ab534870332f1bd7e4df62e3dcb5d10032b
Sam Amant <sam.amant@wegroup.be>
2021-10-20 09:48:42 +0200

    Add option to disable ordering to orderTable

--- src/components/Tables/OrderTable/Head.jsx
+++ src/components/Tables/OrderTable/Head.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
-import { string, object, node } from 'prop-types';
+import { string, object, node, bool } from 'prop-types';
 
-const Head = ({ className, children }) => {
+const Head = ({ className, orderEnabled = true, children }) => {
   const [ActiveOrder, setActiveOrder] = useState(0);
   const [OrderDirection, setOrderDirection] = useState('descending');
 
@@ -25,8 +25,8 @@ const Head = ({ className, children }) => {
       <tr>
         {elements.map((element, index) => {
           return React.cloneElement(element, {
-            onClick: () => clickHandler(index),
-            active: index === ActiveOrder ? true : false,
+            onClick: () => (orderEnabled ? clickHandler(index) : null),
+            active: index === ActiveOrder && orderEnabled ? true : false,
             order: OrderDirection,
           });
         })}
@@ -45,6 +45,8 @@ Head.propTypes = {
   /** Adds extra props to the element */
   otherProps: object,
   children: node,
+  /** Decides if the ordering functionality should be enabled */
+  orderEnabled: bool,
 };
 
 export default Head;
